7b691abbfff4c4085f10373a2bf2a666f8cc03e1
Nanaba Gyamfi <84647110+nanaba4r@users.noreply.github.com>
2024-03-24 13:27:17 -0700

    Update deliver.sh
    debug 2

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,5 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 pwd
-echo "target/${NAME}-${VERSION}.jar"
-ls -larth "target/${NAME}-${VERSION}.jar"
+ls -larth "target/"
 java -jar target/${NAME}-${VERSION}.jar
